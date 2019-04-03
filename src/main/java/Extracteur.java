104e073cd79e1caf574c0f4d12ee003f6f924b60
nathan vignal <37573225+nathan-vignal@users.noreply.github.com>
2019-04-03 16:09:48 +0200

    calcul frequence

--- src/main/java/Extracteur.java
+++ src/main/java/Extracteur.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Extracteur {
-    private Map<ArrayList<Integer>,Float> frequencies;
 
 
 
-    public static Map<ArrayList<Integer>,Float> readData() throws IOException {
+    public static Map<ArrayList<Integer>,Float> readData(int nbElements) throws IOException {
 
         String file = "./src/main/java/csv/test.txt";
         Map<ArrayList<Integer>,Float> content = new HashMap<ArrayList<Integer>,Float>();
@@ -25,7 +24,12 @@ public class Extracteur {
                 ArrayList<Integer> temp = new ArrayList<Integer>();
                 stringContent = line.split(";");
                 for(int i =0; i<stringContent.length-1; ++i){
-                    temp.add( Integer.parseInt(stringContent[i]) );
+                    if(!stringContent[i].equals("")){
+                        temp.add( Integer.parseInt(stringContent[i]) );
+                    }else{
+                        temp.add( 0);
+                    }
+
                 }
                 content.put(temp,Float.parseFloat(stringContent[stringContent.length-1]));
             }
@@ -33,9 +37,85 @@ public class Extracteur {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //for each element in the content map
+        for(Map.Entry<ArrayList<Integer>,Float> entry : content.entrySet()) {
+            content.put(entry.getKey(),entry.getValue()/nbElements);
+
+        }
 
         return content;
     }
+
+    public void patternFinder(Map<ArrayList<Integer>,Float> data){
+
+        for(Map.Entry<ArrayList<Integer>,Float>  row: data.entrySet()) {
+            row.getKey();
+            row.getValue();
+
+
+        }
+
+
+
+    }
+
+    /**
+     * find all combinations with the given numbers
+     * @param data
+     * @return the combinations
+     */
+    public static ArrayList<ArrayList<Integer>> combinationFinder(ArrayList<Integer> data){
+        ArrayList<ArrayList<Integer>> combinations = new ArrayList<ArrayList<Integer>>();
+        //Pour phase
+        for(int iterationNb =0;iterationNb<data.size(); ++iterationNb){
+
+            // pour chaque item
+            for( int itemNb =0; itemNb<data.size()-iterationNb; ++itemNb) {
+                ArrayList<Integer>  sousCombinaison = new  ArrayList<Integer>();
+
+                //préfixe
+                for(int i =itemNb; i<iterationNb+1; ++i) {
+                    sousCombinaison.add(data.get(i));
+
+                }
+                System.out.println(sousCombinaison);
+                combinations.add(sousCombinaison);
+
+
+                /*
+
+                // jusqu'à nb item par combinaison
+                for( int i =0; i< iterationNb+1; ++i){
+
+
+
+                    ArrayList<Integer>  sousCombinaison = new  ArrayList<Integer>();
+                    sousCombinaison.add(data.get(itemNb));
+                    //pour chaque item suivant
+                    for(int j= itemNb+1; j<data.size(); ++j){
+                        sousCombinaison.add(data.get(j));
+                        System.out.println(sousCombinaison);
+
+                    }
+                    combinations.add(sousCombinaison);
+
+
+
+                }
+*/
+            }
+
+        }
+
+
+
+
+
+
+        return combinations;
+    }
+
+
 
 }
 
